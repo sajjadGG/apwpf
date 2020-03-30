04ab1c4df80aa2e9e337f90f21a7ed502423c1eb
sajjad <sajjad_ramezani@ind.iust.ac.ir>
2020-03-30 22:44:45 +0430

    black theme added

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-
+using MahApps.Metro;
 namespace WpfApp1
 {
     /// <summary>
@@ -13,5 +13,20 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        public static bool isBlack = false;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Tuple<AppTheme, Accent> appStyle = ThemeManager.DetectAppStyle(Application.Current);
+
+            ThemeManager.ChangeAppStyle(Application.Current,
+                                        ThemeManager.GetAccent("Cyan"),
+                                        ThemeManager.GetAppTheme("BaseLight"));
+            base.OnStartup(e);
+        }
+        
+
+       
+
+
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro;
 using MahApps.Metro.Controls;
 namespace WpfApp1
 {
@@ -26,6 +27,36 @@ namespace WpfApp1
         {
             InitializeComponent();
             tbcstatic = tbc;
+        }
+
+        private void Tbc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem oldTabItem;
+            try
+            {
+                oldTabItem = e.RemovedItems[0] as TabItem;
+            }catch(Exception ex)
+            {
+                oldTabItem = homeTab;
+            }
+            if (blackItem.IsSelected)
+            {
+                if (!App.isBlack)
+                {
+                    ThemeManager.ChangeAppStyle(Application.Current,
+                                            ThemeManager.GetAccent("Cyan"),
+                                            ThemeManager.GetAppTheme("BaseDark"));
+                    App.isBlack = true;
+                }
+                else
+                {
+                    ThemeManager.ChangeAppStyle(Application.Current,
+                                            ThemeManager.GetAccent("Cyan"),
+                                            ThemeManager.GetAppTheme("BaseLight"));
+                    App.isBlack = false;
+                }
+                tbc.SelectedIndex = 0;
+            }
         }
     }
 }
